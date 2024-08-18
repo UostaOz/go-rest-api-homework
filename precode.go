e9ffb8b77e66c773ec73a5cc67705f1c30a19b4d
Maria Kostetskaya <i.fafa.ru@icloud.com>
2024-08-18 19:05:26 +0300

    first try

--- precode.go
+++ precode.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
-// Task ...
 type Task struct {
-	ID           string   `json:"id"`
-	Description  string   `json:"description"`
-	Note         string   `json:"note"`
-	Applications []string `json:"applications"`
+	ID          string   `json:"id"`          // ID задачи
+	Description string   `json:"description"` // Заголовок
+	Note        string   `json:"note"`        // Описание задачи
+	Application []string `json:"application"` // Приложения, которыми будете пользоваться
 }
 
 var tasks = map[string]Task{
@@ -20,7 +21,7 @@ var tasks = map[string]Task{
 		ID:          "1",
 		Description: "Сделать финальное задание темы REST API",
 		Note:        "Если сегодня сделаю, то завтра будет свободный день. Ура!",
-		Applications: []string{
+		Application: []string{
 			"VS Code",
 			"Terminal",
 			"git",
@@ -30,7 +31,7 @@ var tasks = map[string]Task{
 		ID:          "2",
 		Description: "Протестировать финальное задание с помощью Postmen",
 		Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
-		Applications: []string{
+		Application: []string{
 			"VS Code",
 			"Terminal",
 			"git",
@@ -39,14 +40,84 @@ var tasks = map[string]Task{
 	},
 }
 
-// Ниже напишите обработчики для каждого эндпоинта
-// ...
+// Обработчик для получения всех задач
+func getAll(w http.ResponseWriter, r *http.Request) {
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+// Обработчик для отправки задачи на сервер
+func sendTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Обработчик для получения задачи по ID
+func getId(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Такой задачи нет!", http.StatusNoContent)
+		return
+	}
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+// Обработчик для удаления задачи по ID
+func delTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Такой задачи нет!", http.StatusNoContent)
+		return
+	}
+	delete(tasks, id)
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
 
 func main() {
 	r := chi.NewRouter()
 
-	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getAll)
+	r.Post("/tasks", sendTask)
+	r.Get("/tasks/{id}", getId)
+	r.Delete("/tasks/{id}", delTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
